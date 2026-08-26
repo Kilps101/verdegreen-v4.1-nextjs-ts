f5fd16b2955fc537c32b35b5c2b8cacd2def04bd
Matt Kilpatrick <mjkilp@gmail.com>
2026-08-26 10:06:34 +0100

    Add signin link to homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -67,6 +67,15 @@ export default function Home() {
           >
             Dashboard
           </a>
+
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="/signin"
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            Sign In
+          </a>
         </div>
       </main>
     </div>
