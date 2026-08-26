2c00c181873e04d8d799bea24d1c7907f81df10e
Matt Kilpatrick <mjkilp@gmail.com>
2026-08-26 21:48:02 +0100

    Separate sign up/in/out buttons. Delete better-auth migrations.

---
+++ app/ui/dashboard/sign-buttons.tsx
@@ -0,0 +1,59 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { PowerIcon } from "@heroicons/react/24/outline";
+
+export default async function SignButtons() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return (
+      <form
+        action={async () => {
+          "use server";
+          const { success } = await auth.api.signOut({
+            headers: await headers(),
+          });
+        }}
+      >
+        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <PowerIcon className="w-6" />
+          <div className="hidden md:block">Sign Out</div>
+        </button>
+      </form>
+    );
+  } else {
+    return (
+      <>
+        <form
+          action={async () => {
+            "use server";
+            await auth.api.signUpEmail();
+          }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">
+              <a href="/signup">Sign Up</a>
+            </div>
+          </button>
+        </form>
+
+        <form
+          action={async () => {
+            "use server";
+            await auth.api.signInEmail();
+          }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">
+              <a href="/signin">Sign In</a>
+            </div>
+          </button>
+        </form>
+      </>
+    );
+  }
+}
