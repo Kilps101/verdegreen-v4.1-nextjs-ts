92a5050c3b588a539c697c5a6bf888005f798af4
Matt Kilpatrick <mjkilp@gmail.com>
2026-08-31 20:28:10 +0100

    Remove 'form' element from sign up/in/out buttons completely.

--- app/ui/dashboard/sign-buttons.tsx
+++ app/ui/dashboard/sign-buttons.tsx
@@ -9,50 +9,27 @@ export default async function SignButtons() {
 
   if (session) {
     return (
-      // <form
-      //   action={async () => {
-      //     "use server";
-      //     const { success } = await auth.api.signOut({
-      //       headers: await headers(),
-      //     });
-      //   }}
-      // >
-        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          <PowerIcon className="w-6" />
-          <div className="hidden md:block">Sign Out</div>
-        </button>
-      // </form>
+      <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <PowerIcon className="w-6" />
+        <div className="hidden md:block">Sign Out</div>
+      </button>
     );
   } else {
     return (
       <>
-        {/* <form
-          action={async () => {
-            "use server";
-            await auth.api.signUpEmail();
-          }}
-        > */}
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">
-              <a href="/signup">Sign Up</a>
-            </div>
-          </button>
-        {/* </form> */}
+        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <PowerIcon className="w-6" />
+          <div className="hidden md:block">
+            <a href="/signup">Sign Up</a>
+          </div>
+        </button>
 
-        {/* <form
-          action={async () => {
-            "use server";
-            await auth.api.signInEmail();
-          }}
-        > */}
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">
-              <a href="/signin">Sign In</a>
-            </div>
-          </button>
-        {/* </form> */}
+        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <PowerIcon className="w-6" />
+          <div className="hidden md:block">
+            <a href="/signin">Sign In</a>
+          </div>
+        </button>
       </>
     );
   }
